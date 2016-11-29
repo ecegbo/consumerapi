59fadb197a96aed76a3ea7efb041e702790f9186
ecegbo <ecegbo@gmail.com>
2016-11-29 18:37:11 +0100

    first commit

---
+++ app/consumerapi.js
@@ -0,0 +1,29 @@
+function loadUserProfile(username) {
+
+    const http = require('https');
+    
+    var options = {
+        host: 'api.github.com',
+        path: '/users/' + username,
+        method: 'GET',
+        headers: {'user-agent': 'node.js'}
+    };
+
+    http.get(options, function(resp) {
+      
+      resp.on('data', function(data) {
+        
+        var dataString = data.toString();
+        console.log(dataString);
+
+      });
+      
+    })
+    .on("error", function(e) {
+      
+      console.log("An error occured: " + e.message);
+      
+    });
+}
+loadUserProfile('ecegbo');
+        
